903cb7c2f52ee13425e69d847983653909102125
SandoLando <sandinacan@gmail.com>
2018-10-10 21:29:03 -0400

    Added tmp files directory

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+
+
+
